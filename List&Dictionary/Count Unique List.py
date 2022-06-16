4cf1005dad00bfc124fe94427b846d2bb9c085b1
pavankumarpamidi <107040349+pavankumarpamidi@users.noreply.github.com>
2022-06-16 14:16:29 +0530

    Update Count Unique List.py

--- List&Dictionary/Count Unique List.py
+++ List&Dictionary/Count Unique List.py
@@ -1,4 +1,11 @@
-List= [1,2,3,3,4,5,4,7]
-unique = set(List)
-print(unique)
-print(len(unique))
+input_list = [1, 2, 2, 5, 8, 4, 4, 8,9]
+
+List = []
+count = 0
+
+for item in input_list:
+    if item not in List:
+        count += 1
+        List.append(item)
+
+print("No of unique items are:", count)
